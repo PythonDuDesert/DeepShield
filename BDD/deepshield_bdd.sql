1fcaa8351841ec33116608d71bf52125ac28f923
Olivier YAMMINE <olivier.yammine@gmail.com>
2026-07-13 13:23:08 +0200

    Update deepshield_bdd.sql

--- BDD/deepshield_bdd.sql
+++ BDD/deepshield_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 06 juil. 2026 à 15:19
+-- Généré le : lun. 13 juil. 2026 à 11:22
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `assistance` (
   `admin_notes` longtext,
   `date_response` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   `profile_photo` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'default-avatar.png',
-  `role` tinyint UNSIGNED NOT NULL DEFAULT '3' COMMENT '0=''super admin''\r\n1=''admin''\r\n2=''premium user''\r\n3=''user''',
+  `role` tinyint UNSIGNED NOT NULL DEFAULT '2' COMMENT '0=''admin''\r\n1=''premium user''\r\n2=''user''',
   `is_active` tinyint DEFAULT '1' COMMENT '0=inactif, 1=actif, 2=bloqué (trop de tentatives)',
   `failed_login_attempts` tinyint UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Nombre de tentatives de connexion échouées',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -107,7 +107,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `reset_token_expires` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password_hash`, `first_name`, `last_name`, `profile_photo`, `role`, `is_active`, `failed_login_attempts`, `created_at`, `updated_at`, `last_login`, `last_try_login`, `email_token`, `email_token_expires`, `reset_token`, `reset_token_expires`) VALUES
+(1, 'olivier.yammine@gmail.com', '$2y$10$bDerZejFmniCRLFq8DGthu3UDKI69LjsteZcfgiAgwQfX3PYENv1.', 'Olivier', 'YAMMINE', 'default-avatar.png', 0, 1, 0, '2026-07-13 09:25:28', '2026-07-13 10:56:07', '2026-07-13 10:56:07', NULL, NULL, NULL, NULL, NULL),
+(2, 'omar.tanaradje@gmail.com', '$2y$10$qiZsHIr4t5a6.5Tcuv46MO7sQ2ZivfhICUs2Sl5kRQDjTKksDFyQy', 'Omar', 'TANARADJE', 'default-avatar.png', 0, 1, 0, '2026-07-13 11:21:30', '2026-07-13 11:22:00', NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'lucas.duhoo@gmail.com', '$2y$10$bPkFpvBy27EOi5B2osTC.enabmYQ8KXTWtqc8cyZ2iZZlBm5.rnmS', 'Lucas', 'DUHOO', 'default-avatar.png', 0, 1, 0, '2026-07-13 11:21:52', '2026-07-13 11:22:05', NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -126,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `score` tinyint DEFAULT NULL COMMENT 'Score in %',
   `explinations` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
